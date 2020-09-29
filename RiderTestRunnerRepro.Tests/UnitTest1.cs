f910c7810adb54e5595787663b16e770d941483a
nolan.sedley <nolan.sedley@mindbodyonline.com>
2020-09-29 13:00:22 -0700

    Initial commit

--- RiderTestRunnerRepro.Tests/UnitTest1.cs
+++ RiderTestRunnerRepro.Tests/UnitTest1.cs
@@ -1,14 +1,20 @@
 ﻿using System;
+using System.Web.Http;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication1.Models;
 
 namespace RiderTestRunnerRepro.Tests
 {
     [TestClass]
     public class UnitTest1
     {
+        private ValuesController _controller = new ValuesController();
         [TestMethod]
         public void TestMethod1()
         {
+            _controller.Configuration = new HttpConfiguration();
+            var model = new Model();
+            _controller.Validate(model);
         }
     }
 }
